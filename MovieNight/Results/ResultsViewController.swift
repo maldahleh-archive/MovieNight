64fb95ba4d23948d082a63be3818bb810adfc3f3
Mohammed Al-Dahleh <maldahleh@gmail.com>
2018-01-30 01:30:02 -0500

    Create custom class for Results VC

---
+++ MovieNight/Results/ResultsViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ResultsViewController.swift
+//  MovieNight
+//
+//  Created by Mohammed Al-Dahleh on 2018-01-30.
+//  Copyright © 2018 Mohammed Al-Dahleh. All rights reserved.
+//
+
+import UIKit
+
+class ResultsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
